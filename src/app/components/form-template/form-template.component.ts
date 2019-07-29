c952bc02b03b414af984e0098f85a4afd63f9991
Rubén Segura <rsegrom@gmail.com>
2019-07-29 18:32:32 +0200

    Obtiene y valida datos del formulario

--- src/app/components/form-template/form-template.component.ts
+++ src/app/components/form-template/form-template.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario.model';
+
 
 @Component({
   selector: 'app-form-template',
   templateUrl: './form-template.component.html',
-  styles: []
+  styles: [`
+  .ng-invalid.ng-touched:not(form) {
+    border: 1px solid red;
+  }
+  .ng-valid.ng-touched:not(form) {
+    border: 1px solid green;
+  }
+  `]
 })
 export class FormTemplateComponent implements OnInit {
+
+  usuarioForm: Usuario;
 
   constructor() { }
 
   ngOnInit() {
+    this.usuarioForm = new Usuario();
+  }
+
+  enviarFormulario( formulario: NgForm ) {
+    console.log('Formulario enviado');
+    console.log(formulario);
   }
 
 }
